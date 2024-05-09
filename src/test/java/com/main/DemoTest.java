04876ac103701c52dc5918d38108c4fe3e85d5dc
newclea <114222079+newclea@users.noreply.github.com>
2024-05-09 17:35:47 +0800

    set a basic stucture

---
+++ src/test/java/com/main/DemoTest.java
@@ -0,0 +1,17 @@
+package com.main;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void add() {
+    }
+}
